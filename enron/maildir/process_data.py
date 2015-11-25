e196f0859f421202bfccd6b867dde9e949c7745c
eric <you@example.com>
2015-11-25 20:49:50 +0200

    keep going

--- enron/maildir/process_data.py
+++ enron/maildir/process_data.py
@@ -18,21 +18,29 @@ with open(LIST) as f:
             for line in g:
                 tokens = line.split(':')
                 tokens = tokens[:2]
+                # if tokens[0] != 'Date':
+                #     tokens = tokens[:2]
+                # else:
+                #     the_date = tokens[1:]
+                #     tokens[1] = the_date
+
                 if re.search('$^', line):
                     d = file_dic[g.name]
-                    common_str = g.name + ':' + d['Message-ID'] + ':' + d['From']
+                    common_str = g.name + ':' + d['Date'] + ':' +  d['Message-ID'] + ':' + d['From']
                     for i,val_dic in enumerate(d['to_list']):
                         type = val_dic['type']
-                        value = val_dic['val']
-                        if value.strip():
-                            o.write(common_str + ':' + type + '=' + value + '\n')
+                        value_list = val_dic['val'].split(',')
+                        for j, to_value in enumerate(value_list):
+                            to_value = to_value.strip()
+                            if to_value:
+                                o.write(common_str + ':' + to_value + ':' + type + '\n')
                     if not len(d['to_list']):
                         o.write(common_str)
                     break
                 # print 'tokens[0]: %s' %tokens[0]
-                if re.search('(Cc|Bcc|Message-ID|From|To|X-To|X-cc|X-bcc)$',tokens[0]):
+                if re.search('(Date|Cc|Bcc|Message-ID|From|To)$',tokens[0]):
                     # print 'token is matching, val is %s' %tokens[0]
-                    if re.search('(.*To|X.*|Cc|Bcc)', tokens[0]):
+                    if re.search('^(To|Cc|Bcc)', tokens[0]):
                         file_dic[g.name]['to_list'].append({'type':tokens[0], 'val': tokens[1].strip()})
                     else:
                         file_dic[g.name][tokens[0]] = tokens[1].strip()

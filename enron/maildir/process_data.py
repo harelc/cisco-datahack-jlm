1bc6e59f21c3248e638d9488a3931b391438dee9
eric <you@example.com>
2015-11-25 21:14:19 +0200

    more entries in output.txt

--- enron/maildir/process_data.py
+++ enron/maildir/process_data.py
@@ -27,24 +27,31 @@ with open(LIST) as f:
 
                 if re.search('$^', line):
                     d = file_dic[g.name]
-                    common_str = g.name + ':' + d['Date'] + ':' +  d['Message-ID'] + ':' + d['From']
-                    for i,val_dic in enumerate(d['to_list']):
-                        type = val_dic['type']
-                        value_list = val_dic['val'].split(',')
-                        for j, to_value in enumerate(value_list):
-                            to_value = to_value.strip()
-                            if to_value:
-                                o.write(common_str + ':' + to_value + ':' + type + '\n')
-                    if not len(d['to_list']):
-                        o.write(common_str + '\n')
-                    break
+                    try:
+                        common_str = g.name + ':' + d['Date'] + ':' +  d['Message-ID'] + ':' + d['From']
+                        for i,val_dic in enumerate(d['to_list']):
+                            type = val_dic['type']
+                            value_list = val_dic['val'].split(',')
+                            for j, to_value in enumerate(value_list):
+                                to_value = to_value.strip()
+                                if to_value:
+                                    o.write(common_str + ':' + to_value + ':' + type + '\n')
+                        if not len(d['to_list']):
+                            o.write(common_str + '\n')
+                    except:
+                        pass
+                    finally:
+                        break
                 # print 'tokens[0]: %s' %tokens[0]
                 if re.search('(Date|Cc|Bcc|Message-ID|From|To)$',tokens[0]):
                     # print 'token is matching, val is %s' %tokens[0]
                     if re.search('^(To|Cc|Bcc)', tokens[0]):
                         file_dic[g.name]['to_list'].append({'type':tokens[0], 'val': tokens[1].strip()})
                     else:
-                        file_dic[g.name][tokens[0]] = tokens[1].strip()
+                        try:
+                            file_dic[g.name][tokens[0]] = tokens[1].strip()
+                        except:
+                            pass
                     continue
 
 o.close()

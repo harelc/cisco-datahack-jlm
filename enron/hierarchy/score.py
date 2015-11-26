98f8b017283001be8ea2bdf4f9a4e95733262c44
eric <you@example.com>
2015-11-26 15:02:29 +0200

    code refactoring

--- enron/hierarchy/score.py
+++ enron/hierarchy/score.py
@@ -7,15 +7,27 @@ This is a temporary script file.
 
 import pandas as pd
 
+def get_scrore_by_type(df, tpy, filter_func):
+    df_sub = df[['id','to_type','to_val']]
+    df_type_filtered = df_sub[df_sub['to_type'] == tpy]
+    filtered_by_func = df_type_filtered[df_type_filtered['to_val'].apply(filter_func)]
+    return filtered_by_func['to_val'].value_counts()
+    
+    
+    
+
 def get_cc_score_frame(df):
     df_sub = df[['id','to_type','to_val']]
     df_cc_column = df_sub[df_sub['to_type'] == 'Cc']
-    return df_cc_column['to_val'].value_counts()
+    no_team_cc_column = df_cc_column[df_cc_column['to_val'].apply(lambda x: not x.startswith('team'))]
+    return no_team_cc_column['to_val'].value_counts()
     
+            
 def get_to_score_frame(df):
     df_sub = df[['id','to_type','to_val']]
     df_to_column = df_sub[df_sub['to_type'] == 'To']
-    return df_to_column['to_val'].value_counts()
+    no_team_to_column = df_to_column[df_to_column['to_val'].apply(lambda x: not x.startswith('team'))]
+    return no_team_to_column['to_val'].value_counts()
     
 def get_to_team_score_frame(df):
     df_sub = df[['id','to_type','to_val']]
@@ -32,11 +44,11 @@ def main(n_rows):
     
     
 if __name__ == '__main__':
-    t = main(10000)
+    t = main(100)
     print 'cc_score_frame'
     print t[0]
-    print 'to_score_frame'
-    print t[1]
-    print 'to_team_frame'
-    print t[2]
-    
+#    print 'to_score_frame'
+#    print t[1]
+#    print 'to_team_frame'
+#    print t[2]
+#    

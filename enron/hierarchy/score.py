06a63a8be01998f50037e8d0bedbaddd237b8bf5
eric <you@example.com>
2015-11-26 15:37:09 +0200

    add 4 columns: cc, to, send to team, send cc team

--- enron/hierarchy/score.py
+++ enron/hierarchy/score.py
@@ -7,46 +7,55 @@ This is a temporary script file.
 
 import pandas as pd
 
-def get_scrore_by_type(df, tpy, filter_func):
-    df_sub = df[['id','to_type','to_val']]
+def get_score_by_type(df, tpy, filter_func, column='to_val'):
+    df_sub = df[['id','to_type', 'from','to_val']]
     df_type_filtered = df_sub[df_sub['to_type'] == tpy]
     filtered_by_func = df_type_filtered[df_type_filtered['to_val'].apply(filter_func)]
-    return filtered_by_func['to_val'].value_counts()
+    return filtered_by_func[column].value_counts()
     
     
     
 
 def get_cc_score_frame(df):
-    df_sub = df[['id','to_type','to_val']]
-    df_cc_column = df_sub[df_sub['to_type'] == 'Cc']
-    no_team_cc_column = df_cc_column[df_cc_column['to_val'].apply(lambda x: not x.startswith('team'))]
-    return no_team_cc_column['to_val'].value_counts()
-    
-            
+     return get_score_by_type(df, 'Cc',lambda x: not x.startswith('team'))
+
 def get_to_score_frame(df):
-    df_sub = df[['id','to_type','to_val']]
-    df_to_column = df_sub[df_sub['to_type'] == 'To']
-    no_team_to_column = df_to_column[df_to_column['to_val'].apply(lambda x: not x.startswith('team'))]
-    return no_team_to_column['to_val'].value_counts()
+     return get_score_by_type(df, 'To',lambda x: not x.startswith('team'))    
     
-def get_to_team_score_frame(df):
-    df_sub = df[['id','to_type','to_val']]
-    df_to_column = df_sub[df_sub['to_type'] == 'To']
-    df_to_team_column = df_to_column[df_to_column['to_val'].str.startswith("team")]
-    return df_to_team_column['to_val'].value_counts()
+def get_cc_team_sender_score_frame(df):
+     return get_score_by_type(df, 'Cc',lambda x: x.startswith('team'), 'from')    
+
+def get_to_team_sender_score_frame(df):
+     return get_score_by_type(df, 'To',lambda x: x.startswith('team'), 'from')    
 
 def main(n_rows):
     df = pd.read_csv('data/output.csv',nrows=n_rows, names=['path','date','id','from','to_val','to_type'])
     cc_score_frame = get_cc_score_frame(df)
+    print 'cc_score_frame[:10]'
+    print cc_score_frame[:10]
+    print '++++++++++++++++++++++++++++++++++++'    
     to_score_frame = get_to_score_frame(df)
-    to_team_score_frame = get_to_team_score_frame(df)
-    return (cc_score_frame,to_score_frame,to_team_score_frame)
+    print to_score_frame[:10]    
+    print '++++++++++++++++++++++++++++++++++++'    
+    cc_team_score_frame = get_cc_team_sender_score_frame(df)
+    print cc_team_score_frame[:10]    
+    print '++++++++++++++++++++++++++++++++++++'    
+    to_team_score_frame = get_to_team_sender_score_frame(df)
+    print to_team_score_frame[:10]    
+    print '++++++++++++++++++++++++++++++++++++'    
+    df_result = pd.DataFrame({"cc_score_frame": cc_score_frame, "to_score_frame": to_score_frame, 
+                              "cc_team_score_frame": cc_team_score_frame, "to_team_score_frame": to_team_score_frame})
+    return df_result    
+    #return (cc_score_frame,to_score_frame,to_team_score_frame)
     
     
 if __name__ == '__main__':
-    t = main(100)
+    t = main(100000)
     print 'cc_score_frame'
-    print t[0]
+    print t.loc['william.kendrick@enron.com']
+    print t.loc['larry.campbell@enron.com']
+#    print t.iloc[:10]
+    print len(t)
 #    print 'to_score_frame'
 #    print t[1]
 #    print 'to_team_frame'

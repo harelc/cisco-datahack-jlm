aa5853b333892d1dd259b1de2603e4b3de2fb41d
eric <you@example.com>
2015-11-26 14:20:53 +0200

    continue feature extraction

--- enron/hierarchy/score.py
+++ enron/hierarchy/score.py
@@ -7,27 +7,36 @@ This is a temporary script file.
 
 import pandas as pd
 
-df = pd.read_csv('output.csv',nrows=10000, names=['path','date','id','from','to_val','to_type'])
+def get_cc_score_frame(df):
+    df_sub = df[['id','to_type','to_val']]
+    df_cc_column = df_sub[df_sub['to_type'] == 'Cc']
+    return df_cc_column['to_val'].value_counts()
+    
+def get_to_score_frame(df):
+    df_sub = df[['id','to_type','to_val']]
+    df_to_column = df_sub[df_sub['to_type'] == 'To']
+    return df_to_column['to_val'].value_counts()
+    
+def get_to_team_score_frame(df):
+    df_sub = df[['id','to_type','to_val']]
+    df_to_column = df_sub[df_sub['to_type'] == 'To']
+    df_to_team_column = df_to_column[df_to_column['to_val'].str.startswith("team")]
+    return df_to_team_column['to_val'].value_counts()
 
-print 'create sub df'
-
-df_1 = df[['id','to_type','to_val']]
-#print df_1
-df_2 = df_1[df_1['to_type'] == 'Cc']
-
-print '$$$$$$$$$$$$$$$$$$$$$$$$$$'
-#print df_2
-
-print df_2['to_val'].value_counts()
-
-quit()
-
-print '======================='
-g =  df_1.groupby(['id'])
-for member in g:
-    print member
-print '======================='
-
-df_group = df_1.groupby(['id','to_val']).count()
-print df_group
-
+def main(n_rows):
+    df = pd.read_csv('data/output.csv',nrows=n_rows, names=['path','date','id','from','to_val','to_type'])
+    cc_score_frame = get_cc_score_frame(df)
+    to_score_frame = get_to_score_frame(df)
+    to_team_score_frame = get_to_team_score_frame(df)
+    return (cc_score_frame,to_score_frame,to_team_score_frame)
+    
+    
+if __name__ == '__main__':
+    t = main(10000)
+    print 'cc_score_frame'
+    print t[0]
+    print 'to_score_frame'
+    print t[1]
+    print 'to_team_frame'
+    print t[2]
+    
